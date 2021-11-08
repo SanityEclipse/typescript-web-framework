176cb65984b106d269b05333742ce37320784e73
SanityEclipsecd <thomasnrizzo@gmail.com>
2021-11-07 18:28:30 -0700

    get/set event stubs

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,23 @@
-export class User {
-  constructor(private data: UserProps) { }
-
-  get(propName: string): (number | string) {
-    return this.data[propName];
-  }
-}
+// callback recieves no args, returns no values
+type Callback = () => {};
 
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
+}
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback) {}
+
+  trigger(eventName: string): void {}
 }
