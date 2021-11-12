6c465eb63a8337af4f31db3899cd8929e050cab7
Thomas N Rizzo III <thomas.rizzoiii@centurylink.com>
2021-11-12 15:38:28 -0700

    Added 'Collection' class and User implentation of Collection class.

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,6 +3,7 @@ import { BaseModel } from './BaseModel';
 import { ApiSync } from './ApiSync';
 import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 import UserProps from '../interfaces/UserProps';
 
 const rootUrl = 'http://localhost:3000';
@@ -14,5 +15,12 @@ export class User extends BaseModel<UserProps>{
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
     )
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(
+      rootUrl,
+      (json: UserProps) => User.buildUser(json)
+    );
   }
 }
